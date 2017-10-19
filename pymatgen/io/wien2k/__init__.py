42edda53d6f1fd5ca1ee0bd0becb698801d4bad8
Eamon MCDERMOTT EM250772 <eamon.mcdermott@cea.fr>
2017-10-19 18:18:53 +0200

    initial wien2k commit

---
+++ pymatgen/io/wien2k/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Copyright (c) Eamon McDermott / CEA LETI.
+# Distributed under the terms of the MIT License.
+
+from __future__ import unicode_literals
+
+"""
+This package implements modules for input and output to and from WIEN2k. It
+is based on the VASP module
+"""
+
+from .inputs import *
+from .outputs import *
