95db48c3cb91e779dd6ab02705d3509ea3345f56
Eamon McDermott <github@jackmaninov.ca>
2019-04-10 15:49:22 +0200

    cleanups

--- pymatgen/io/wien2k/firetasks.py
+++ pymatgen/io/wien2k/firetasks.py
@@ -12,14 +12,17 @@ __maintainer__ = 'Shyue Ping Ong'
 __email__ = 'ongsp@ucsd.edu'
 __date__ = 'Apr 10, 2019'
 
+
 class ArchiveWIEN2kOutputTask(FiretaskBase):
     """
-    Load a WIEN2k output file (using classes from outputs.py) and store it in stored_data
+    Load a WIEN2k output file (using classes from outputs.py) and store
+    it in stored_data
 
     Args:
         base_name (str): Name of the file to create, including the path,
             minus any format-specific extension.
-        format (str): Optional. one of "zip", "tar", "bztar" or "gztar". Defaults to gztar.
+        format (str): Optional. one of "zip", "tar", "bztar" or "gztar".
+        Defaults to gztar.
     """
 
     _fw_name = 'ArchiveDirTask'
@@ -27,6 +30,6 @@ class ArchiveWIEN2kOutputTask(FiretaskBase):
     optional_params = ["format"]
 
     def run_task(self, fw_spec):
-        # shutil.make_archive(self["base_name"], format=self.get("format", "gztar"), root_dir=".")
+        print(self.get("format"))
         toSave = self.get("format").from_file(self.get("output_file"))
         return FWAction(stored_data=toSave.as_dict())
