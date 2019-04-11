1fa36b9a0741bb47c9e30e88dbbfcc3370c8cf5d
Eamon McDermott <github@jackmaninov.ca>
2019-04-11 13:31:35 +0300

    test EelsAngleSweepTask()

--- pymatgen/io/wien2k/firetasks.py
+++ pymatgen/io/wien2k/firetasks.py
@@ -3,7 +3,9 @@
 from __future__ import unicode_literals
 
 from fireworks.core.firework import FiretaskBase, FWAction
+from fireworks import Firework, Workflow, ScriptTask, FileTransferTask
 import pymatgen.io.wien2k.outputs
+from pymatgen.io.wien2k.inputs import Innes
 
 __author__ = 'Eamon McDermott'
 __copyright__ = 'Copyright 2019'
@@ -19,7 +21,7 @@ class ArchiveWIEN2kOutputTask(FiretaskBase):
     it in stored_data
 
     Args:
-        base_name (str): Name of the file to create, including the path,
+        output_file (str): Name of the file to create, including the path,
             minus any format-specific extension.
         format (str): Optional. one of "zip", "tar", "bztar" or "gztar".
         Defaults to gztar.
@@ -32,3 +34,89 @@ class ArchiveWIEN2kOutputTask(FiretaskBase):
     def run_task(self, fw_spec):
         toSave = eval(self.get("format")).from_file(self.get("output_file"))
         return FWAction(stored_data=toSave.as_dict())
+
+class DeployInessInputTask(FiretaskBase):
+    """
+    Write a WIEN2k Innes input file from spec
+
+    Args:
+        case_name (str): Name of file to create
+        Innes (dict): Innes to create
+    """
+
+    _fw_name = 'DeployInessInputTask'
+    required_params = ["output_file"]
+    optional_params = ["Innes"]   #TODO do I pass a dict or just what I want to change?
+
+    def run_task(self, fw_spec):
+        a = Innes.from_dict(self.get("Innes"))
+        a.write_file(self.get("output_file"))
+
+class TelnesRunTask(FiretaskBase):
+    """
+    Run TELNES given an Innes spec
+
+    Args:
+        case_name (str): Name of case (no extension)
+        origin (str): originating directory to copy
+        Innes (dict): Innes spec
+    """
+
+    _fw_name = 'TelnesRunTask'
+    required_params = ["case_name", "origin"]
+    optional_params = ["Innes"]
+
+    def TelnesList(case='case', path=''):
+        TelnesExtensions = ['kgen', 'innes', 'inc', 'inb', 'corewavef', 'final', 'vsp', 'vtotal',
+                            'struct', 'qtl', 'rotij', 'ortho', 'ctr', 'sdlm', 'matrix', 'cdos', 'dos',
+                            'xdos', 'sp2', 'angular', 'eelstable']
+        return ([path + (case + '.' + ext) for ext in TelnesExtensions])
+
+    def run_task(self, fw_spec):
+        case = self.get("case_name")
+        mkdirTask = ScriptTask.from_str('mkdir ' + case)
+        deployTask = FileTransferTask(
+            {'files': TelnesList(case, self.get("origin")), 'dest': case,
+             'mode': 'copy'})  # , 'ignore_errors':'True'
+        deployTask2 = DeployInessInputTask({'output_file': case + '/' + case + ".innes", 'Innes': self.get("Innes")})
+        runTask = ScriptTask.from_str('cd '+ case +' && x telnes3 && x broadening >> STDOUT')
+        archiveTask = ArchiveWIEN2kOutputTask( {'output_file': case + '/' + case + '.broadspec',
+                                                'format': 'pymatgen.io.wien2k.outputs.Eels'})
+        cleanTask = ScriptTask.from_str('yes|rm -rf '+case)
+        return FWAction(additions=Firework([mkdirTask, deployTask, deployTask2, runTask, archiveTask, cleanTask]))
+
+class EelsAngleSweepTask(FiretaskBase):
+    """
+    Generate all Eels output over a range of angles. Run a TelnesRunTasks for each
+
+    Args:
+        case_name (str): Name of the WIEN2k case (no extension)
+        origin (str): originating directory to copy
+        Innes (dict): Base Innes spec (angles will be overwritten)
+        step (int): step size
+    """
+
+    _fw_name = 'EelsAngleSweep'
+    required_params = ["case_name", "origin", "Innes"]
+    optional_params = ["step", "phase", "component", "method", "optimized"]
+
+    def frange(start, stop, step):
+        i = start
+        while i <= stop:
+            yield i
+            i += step
+
+    def run_task(self, fw_spec):
+        step = int(self.get("step"))
+        if not step: step = 15
+
+        tasklist = Firework()
+
+        baseInnes = Innes.from_dict(self.get("Innes"))
+        for alpha in self.frange(0, 90 , step):
+            for beta in self.frange(0, 90, step):
+                for gamma in self.frange(0, 90, step):
+                    baseInnes.config_dict["ORIENTATION SENSITIVE"] = [alpha, beta, gamma]
+                    tasklist.tasks.append(TelnesRunTask({"case_name": self.get("case_name"), "origin": self.get("origin")
+                                                         "Innes": baseInnes.as_dict()}))
+        return FWAction(additions=tasklist)
